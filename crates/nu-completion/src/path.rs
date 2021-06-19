a696e3aed53159871a934d2be8f6211ba2e53067
Niklas Jonsson <niklasandersjonsson@gmail.com>
2021-06-19 20:57:56 +0200

    fix win

--- crates/nu-completion/src/path.rs
+++ crates/nu-completion/src/path.rs
@@ -17,23 +17,31 @@ pub struct PathSuggestion {
 
 impl PathCompleter {
     pub fn path_suggestions(&self, partial: &str, matcher: &dyn Matcher) -> Vec<PathSuggestion> {
-        let (base_dir_name, partial) = partial.rsplit_once(is_separator).unwrap_or((".", partial));
+        dbg!(&partial);
+        let (base_dir_name, partial) = {
+            let (base, rest) = partial.rsplit_once(is_separator).unwrap_or((".", partial));
+            // On windows, this standardizes paths to use \
+            let mut base = base.replace(is_separator, &SEP.to_string());
 
-        // On windows, this standardizes paths to use \
-        let mut base_dir_name = base_dir_name.replace(is_separator, &SEP.to_string());
-
-        // rsplit_once removes the separator
-        base_dir_name.push(SEP);
+            // rsplit_once removes the separator
+            base.push(SEP);
+            (base, rest)
+        };
 
         let base_dir = nu_path::expand_path(Cow::Borrowed(Path::new(&base_dir_name)));
+        dbg!(&base_dir);
+        if !base_dir.is_dir() {
+            return Vec::new();
+        }
 
-        if let Ok(result) = base_dir.read_dir() {
+        let r = if let Ok(result) = base_dir.read_dir() {
             result
                 .filter_map(|entry| {
                     entry.ok().and_then(|entry| {
                         let mut file_name = entry.file_name().to_string_lossy().into_owned();
                         if matcher.matches(partial, file_name.as_str()) {
                             let mut path = format!("{}{}", &base_dir_name, file_name);
+                            dbg!(&path);
                             if entry.file_type().map(|ft| ft.is_dir()).unwrap_or(false) {
                                 path.push(SEP);
                                 file_name.push(SEP);
@@ -54,7 +62,8 @@ impl PathCompleter {
                 .collect()
         } else {
             Vec::new()
-        }
+        };
+        dbg!(r)
     }
 }
 
